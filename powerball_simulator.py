41bb37810afc8cb92341d33b26ffa479ee87c406
jjggu97 <jjggu@naver.com>
2024-01-30 15:32:26 +0900

    1/30

--- powerball_simulator.py
+++ powerball_simulator.py
@@ -7,4 +7,21 @@ def generate_powerball_ticket():
 
 def simulate_powerball_tickets(num_tickets):
     tickets = []
+    for _ in range(num_tickets):
+        ticket =generate_powerball_ticket()
+        tickets.append(ticket)
+
+    return tickets
+
+def main():
+    num_tickets = int(input("Enter the number of tickets to buy :"))
+    tickets = simulate_powerball_tickets(num_tickets)
+    print("\nPurchased tickets : ")
+
+    for idx, ticket in enumerate(tickets, start=1):
+        white_balls, power_ball = ticket
+        print(f"Ticket {idx}: {white_balls}, Power ball : {power_ball}")
+
     
+if __name__ == "__main__":
+    main()
