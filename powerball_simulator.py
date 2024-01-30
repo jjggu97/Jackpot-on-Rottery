b1047ab9f075f6e18a17e6691ccdfca0e5df0718
jjggu97 <jjggu@naver.com>
2024-01-31 04:17:13 +0900

    1/31

--- powerball_simulator.py
+++ powerball_simulator.py
@@ -1,8 +1,8 @@
 import random
 
 def generate_powerball_ticket():
-    white_balls = random.sample(range(1, 70), 5)  # 1부터 69 사이의 5개의 서로 다른 숫자 선택
-    power_ball = random.randint(1, 26)            # 1부터 26 사이의 파워볼 번호 선택
+    white_balls = random.sample(range(1, 70), 5)  
+    power_ball = random.randint(1, 26)          
     return sorted(white_balls), power_ball
 
 def simulate_powerball_tickets(num_tickets):
@@ -13,12 +13,12 @@ def simulate_powerball_tickets(num_tickets):
     return tickets
 
 def main():
-    num_tickets = int(input("구매할 티켓 수를 입력하세요: "))
+    num_tickets = int(input("Enter number of ticket : "))
     tickets = simulate_powerball_tickets(num_tickets)
-    print("\n구매한 티켓:")
+    print("\nTicket Bought:")
     for idx, ticket in enumerate(tickets, start=1):
         white_balls, power_ball = ticket
-        print(f"티켓 {idx}: {white_balls}, 파워볼: {power_ball}")
+        print(f"Ticket {idx}: {white_balls}, Power ball: {power_ball}")
 
 if __name__ == "__main__":
     main()
